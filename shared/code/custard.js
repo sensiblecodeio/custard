8425ab4b8467d6e31d15fa84e76399977938cda0
Zarino Zappia <mail@zarino.co.uk>
2012-11-08 09:52:39 +0000

    removed unused "responsive" javascript

--- shared/code/custard.js
+++ shared/code/custard.js
@@ -13,10 +13,6 @@
 $(function(){
     $('body[data-stripes]').stripes();
 
-    $('<a>').attr('id', 'toggle-nav').text('Toggle navigation').attr('title', 'Toggle navigation').on('click', function(){
-        $(this).next().toggleClass('hidden');
-    }).insertAfter('#masthead h1');
-
     $('.tool #masthead h1').append('<i class="icon-chevron-left"></i>');
 
 });

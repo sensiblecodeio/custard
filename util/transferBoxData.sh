d1b9d07a7ac78dbc891451ced476bebb4f6cc504
Chris Blower <chris.blower@jumpwire.co.uk>
2013-05-28 16:07:01 +0000

    rsync

--- util/transferBoxData.sh
+++ util/transferBoxData.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 BOX_NAME="$1"
-duplicity --s3-use-new-style --file-to-restore=$BOX_NAME restore s3+http://cobalt-home /home
+#duplicity --s3-use-new-style --file-to-restore=$BOX_NAME restore s3+http://cobalt-home /home/$BOX_NAME
+
+rsync --archive --verbose -e 'ssh -i /tmp/something'  root@box.scraperwiki.com:/home/$BOX_NAME/ /home/$BOX_NAME

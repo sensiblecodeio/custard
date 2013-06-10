ed857ac637579d43b860113b00a17ce69c0162cb
ehg <chris.blower@jumpwire.co.uk>
2013-06-10 10:49:58 +0100

    use CO_STORAGE_DIR

--- util/transferBoxData.sh
+++ util/transferBoxData.sh
@@ -2,5 +2,4 @@
 
 BOX_NAME="$1"
 SERVER="$2"
-#duplicity --s3-use-new-style --file-to-restore=$BOX_NAME restore s3+http://cobalt-home /home/$BOX_NAME
-rsync --archive --verbose -e 'ssh -i /tmp/something'  root@${SERVER}:/home/$BOX_NAME/ /home/$BOX_NAME
+rsync --archive --verbose -e 'ssh -i /tmp/something' root@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME

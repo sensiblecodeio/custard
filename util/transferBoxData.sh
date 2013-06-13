946a8bfea34f621e8256274e9cd0a2fe7603d320
David Jones <drj@pobox.com>
2013-06-13 16:17:06 +0100

    Use ubuntu user when ssh'ing into ec2 servers.

--- util/transferBoxData.sh
+++ util/transferBoxData.sh
@@ -4,8 +4,7 @@ BOX_NAME="$1"
 SERVER="$2"
 case $SERVER in
   (*ec2*)
-    echo "I'm sorry Dave, I don't know how to migrate from an ec2 server..."
-    rsync --archive --verbose -e 'ssh' --rsync-path 'sudo rsync' root@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME
+    rsync --archive --verbose -e 'ssh' --rsync-path 'sudo rsync' ubuntu@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME
     ;;
   (*)
     rsync --archive --verbose -e 'ssh -i /tmp/something' root@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME

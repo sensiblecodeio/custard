fa27c10b9417aa80f9ba495f529d6a4e15abd1a6
David Jones <drj@pobox.com>
2013-06-13 15:42:28 +0100

    So that we can experiment with migrating from an ec2 boxecutor.

--- util/transferBoxData.sh
+++ util/transferBoxData.sh
@@ -2,4 +2,12 @@
 
 BOX_NAME="$1"
 SERVER="$2"
-rsync --archive --verbose -e 'ssh -i /tmp/something' root@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME
+case $SERVER in
+  (*ec2*)
+    echo "I'm sorry Dave, I don't know how to migrate from an ec2 server..."
+    rsync --archive --verbose -e 'ssh' --rsync-path 'sudo rsync' root@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME
+    ;;
+  (*)
+    rsync --archive --verbose -e 'ssh -i /tmp/something' root@${SERVER}:/home/$BOX_NAME/ ${CO_STORAGE_DIR}/home/$BOX_NAME
+    ;;
+esac

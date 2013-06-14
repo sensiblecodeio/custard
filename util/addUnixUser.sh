8a287acc44056d564e658bba59b12887aeaeaf2a
ehg <chris.blower@jumpwire.co.uk>
2013-06-14 13:21:54 +0100

    migrate script should use CO_STORAGE_DIR to avoid symlink unlinking

--- util/addUnixUser.sh
+++ util/addUnixUser.sh
@@ -9,8 +9,8 @@ passwd_row="${USERNAME}:x:${UID}:${gid}::/home:/bin/bash"
 shadow_row="${USERNAME}:x:15607:0:99999:7:::"
 (
   flock -w 2 9 || exit 99
-  { cat /etc/passwd ; echo "$passwd_row" ; } > /etc/passwd+
-  mv /etc/passwd+ /etc/passwd
-  { cat /etc/shadow ; echo "$shadow_row" ; } > /etc/shadow+
-  mv /etc/shadow+ /etc/shadow
-) 9>/etc/passwd.cobalt.lock
+  { cat ${CO_STORAGE_DIR}/etc/passwd ; echo "$passwd_row" ; } > ${CO_STORAGE_DIR}/etc/passwd+
+  mv ${CO_STORAGE_DIR}/etc/passwd+ ${CO_STORAGE_DIR}/etc/passwd
+  { cat ${CO_STORAGE_DIR}/etc/shadow ; echo "$shadow_row" ; } > ${CO_STORAGE_DIR}/etc/shadow+
+  mv ${CO_STORAGE_DIR}/etc/shadow+ ${CO_STORAGE_DIR}/etc/shadow
+) 9> ${CO_STORAGE_DIR}/etc/passwd.cobalt.lock

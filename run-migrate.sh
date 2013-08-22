96fd47d89ddffe31ee9cc3d10c3053b781521bf7
Peter Waller <peter@scraperwiki.com>
2013-08-22 11:43:24 +0100

    Beef up run-migrate.sh

--- run-migrate.sh
+++ run-migrate.sh
@@ -1,4 +1,27 @@
 
 export NODE_ENV=cron
 
-util/listGitURLsOfDir /ebs/home | grep -v newdataset | sort -u | sudo xargs -n3 ./migrate.sh
+sudo tee /tmp/migrate.log < /dev/null
+
+mkfifo /tmp/sw-pipe
+trap "wait" EXIT
+trap "exec 1>&-" EXIT
+trap "rm /tmp/sw-pipe" EXIT
+
+exec 3>&1
+exec 4>&2
+
+tee -a /tmp/migrate.log < /tmp/sw-pipe >&3 &
+exec 1>/tmp/sw-pipe
+exec 2>&1
+
+util/listGitURLsOfDir /ebs/home | grep -v newdataset | sort -u |
+  while read -r BOX GIT_URL TOOLNAME
+  do
+    if ./should-migrate.sh $BOX $GIT_URL $TOOLNAME &>> /tmp/migrate.log
+    then
+      echo ---- Migrating box $BOX -- $TOOLNAME
+    else
+      echo ---- Skipping box $BOX -- $TOOLNAME
+    fi
+  done

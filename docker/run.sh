6b70499033bfd189f6107844b047dca88f0abdd9
Peter Waller <peter@scraperwiki.com>
2013-12-09 15:14:23 +0000

    First attempt at custard in docker

---
+++ docker/run.sh
@@ -0,0 +1,16 @@
+source ../../swops-secret/keys.sh
+
+ENVS="$(env | grep CU_ | sed 's/^/-e /')"
+# Don't quote $ENVS.
+SCRIPT='
+cd /opt/custard
+npm install --unsafe-perm
+source activate
+echo Starting redis.
+redis-server &> /dev/null &
+echo Starting mongo
+mkdir -p /data/db
+mongod &
+cake dev
+'
+docker run -p 3001:3001 $ENVS -v $(pwd)/..:/opt/custard -t -i custard bash -c "$SCRIPT"
